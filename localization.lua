17c723ae499ae9d2a1cf34a0a09fde54d19df3d6
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2019-10-26 08:34:44 +0300

    Add files via upload

---
+++ localization.lua
@@ -0,0 +1,90 @@
+-- English
+
+-- MonkeyClock config variables descritions
+MONKEYCLOCK_GENERAL_OPTIONS				= "General Options:"
+MONKEYCLOCK_shown_DESC					= "Enable MonkeyClock"
+MONKEYCLOCK_frameColour_DESC			= "Frame Color"
+MONKEYCLOCK_showBorder_DESC				= "Display the border"
+MONKEYCLOCK_borderColour_DESC			= "Border Color"
+MONKEYCLOCK_frameLocked_DESC			= "Lock the frame position"
+MONKEYCLOCK_use24_DESC					= "Use 24 hour format"
+MONKEYCLOCK_useChatAlarm_DESC			= "Use the chat alarm notification"
+MONKEYCLOCK_useDialogAlarm_DESC			= "Use the dialog alarm notification"
+MONKEYCLOCK_MISC_OPTIONS				= "Miscellaneous Options:"
+MONKEYCLOCK_rightClickOpensConfig_DESC	= "Right mouse click opens the config frame"
+MONKEYCLOCK_hourOffset_DESC				= "Hour offset"
+MONKEYCLOCK_minuteOffset_DESC			= "Minute offset"
+MONKEYCLOCK_alarmHour_DESC				= "The alarm hour"
+MONKEYCLOCK_alarmMinute_DESC			= "The alarm minute"
+MONKEYCLOCK_snoozeMinutes_DESC			= "The amount of snooze minutes"
+
+MONKEYCLOCK_CONFIRM_ALARM				= "The MonkeyClock alarm has gone off."
+MONKEYCLOCK_SNOOZE						= "Snooze"
+
+if (GetLocale() == "zhTW") then
+
+MONKEYCLOCK_GENERAL_OPTIONS				= "一般選項"
+MONKEYCLOCK_shown_DESC					= "啟用MonkeyClock"
+MONKEYCLOCK_frameColour_DESC			= "視窗顏色"
+MONKEYCLOCK_showBorder_DESC				= "顯示邊框"
+MONKEYCLOCK_borderColour_DESC			= "邊框顏色"
+MONKEYCLOCK_frameLocked_DESC			= "鎖定視窗"
+MONKEYCLOCK_use24_DESC					= "使用24小時制"
+MONKEYCLOCK_useChatAlarm_DESC			= "在聊天框提醒鬧鐘響了"
+MONKEYCLOCK_useDialogAlarm_DESC			= "彈出對話框提醒鬧鐘響了"
+MONKEYCLOCK_MISC_OPTIONS				= "細節選項："
+MONKEYCLOCK_rightClickOpensConfig_DESC	= "右鍵點擊視窗開啟設定選項"
+MONKEYCLOCK_hourOffset_DESC				= "小時偏移"
+MONKEYCLOCK_minuteOffset_DESC			= "分鐘偏移"
+MONKEYCLOCK_alarmHour_DESC				= "設定鬧鐘的時"
+MONKEYCLOCK_alarmMinute_DESC			= "設定鬧鐘的分"
+MONKEYCLOCK_snoozeMinutes_DESC			= "設定貪睡時間"
+
+MONKEYCLOCK_CONFIRM_ALARM				= "MonkeyClock鬧鐘響了。"
+MONKEYCLOCK_SNOOZE						= "再打個盹"
+
+elseif (GetLocale() == "ruRU") then
+
+MONKEYCLOCK_GENERAL_OPTIONS				= "Общие Настройки:"
+MONKEYCLOCK_shown_DESC					= "Включить MonkeyClock"
+MONKEYCLOCK_frameColour_DESC			= "Цвет окна"
+MONKEYCLOCK_showBorder_DESC				= "Показать границу"
+MONKEYCLOCK_borderColour_DESC			= "Цвет границы"
+MONKEYCLOCK_frameLocked_DESC			= "Блокировка положения окна"
+MONKEYCLOCK_use24_DESC					= "Используйте 24-часовой формат"
+MONKEYCLOCK_useChatAlarm_DESC			= "Используйте чат-уведомление о тревоге"
+MONKEYCLOCK_useDialogAlarm_DESC			= "Используйте диалоговое уведомление о тревоге"
+MONKEYCLOCK_MISC_OPTIONS				= "Разные Настройки:"
+MONKEYCLOCK_rightClickOpensConfig_DESC	= "ПКМ открывает окно конфигурации"
+MONKEYCLOCK_hourOffset_DESC				= "Смещение часов"
+MONKEYCLOCK_minuteOffset_DESC			= "Смещение минут"
+MONKEYCLOCK_alarmHour_DESC				= "Настройка будильника, часы"
+MONKEYCLOCK_alarmMinute_DESC			= "Настройка будильника, минуты"
+MONKEYCLOCK_snoozeMinutes_DESC			= "Количество минут повтора"
+
+MONKEYCLOCK_CONFIRM_ALARM				= "MonkeyClock сработал сигнал."
+MONKEYCLOCK_SNOOZE						= "Вздремнуть"
+
+elseif (GetLocale() == "zhCN") then
+
+MONKEYCLOCK_GENERAL_OPTIONS				= "一般选项"
+MONKEYCLOCK_shown_DESC					= "启用MonkeyClock"
+MONKEYCLOCK_frameColour_DESC			= "窗口颜色"
+MONKEYCLOCK_showBorder_DESC				= "显示边框"
+MONKEYCLOCK_borderColour_DESC			= "边框颜色"
+MONKEYCLOCK_frameLocked_DESC			= "锁定窗口"
+MONKEYCLOCK_use24_DESC					= "使用24小时制"
+MONKEYCLOCK_useChatAlarm_DESC			= "在聊天框提醒闹钟响了"
+MONKEYCLOCK_useDialogAlarm_DESC			= "弹出对话框提醒闹钟响了"
+MONKEYCLOCK_MISC_OPTIONS				= "细节选项："
+MONKEYCLOCK_rightClickOpensConfig_DESC	= "右键点击开启配置选项"
+MONKEYCLOCK_hourOffset_DESC				= "小时偏移"
+MONKEYCLOCK_minuteOffset_DESC			= "分钟偏移"
+MONKEYCLOCK_alarmHour_DESC				= "设定闹钟的时"
+MONKEYCLOCK_alarmMinute_DESC			= "设定闹钟的分"
+MONKEYCLOCK_snoozeMinutes_DESC			= "设定贪睡时间"
+
+MONKEYCLOCK_CONFIRM_ALARM				= "MonkeyClock闹钟响了。"
+MONKEYCLOCK_SNOOZE						= "再打个盹"
+
+end
